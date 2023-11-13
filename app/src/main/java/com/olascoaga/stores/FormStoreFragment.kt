37bf77d898eee12f8eea887ebae2f24c1d74cf88
Uriel <uriel1396@gmail.com>
2023-11-13 09:52:05 -0600

    Add functionality to appbar buttons

--- app/src/main/java/com/olascoaga/stores/FormStoreFragment.kt
+++ app/src/main/java/com/olascoaga/stores/FormStoreFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.snackbar.Snackbar
 import com.olascoaga.stores.databinding.FragmentFormStoreBinding
 
 class FormStoreFragment : Fragment() {
     private lateinit var mBinding: FragmentFormStoreBinding
+    private var mActivity: MainActivity? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,15 +26,43 @@ class FormStoreFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val activity = activity as? MainActivity
-        activity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        activity?.supportActionBar?.title = getString(R.string.form_store_title_add)
+        mActivity = activity as? MainActivity
+        mActivity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mActivity?.supportActionBar?.title = getString(R.string.form_store_title_add)
 
         setHasOptionsMenu(true)
+
+        mBinding.btnSave.setOnClickListener {
+            Snackbar.make(
+                mBinding.root,
+                getString(R.string.form_store_message_success),
+                Snackbar.LENGTH_SHORT
+            ).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_save, menu)
         super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            android.R.id.home -> {
+                mActivity?.onBackPressed()
+                true
+            }
+            R.id.action_save -> {
+                Snackbar.make(
+                    mBinding.root,
+                    getString(R.string.form_store_message_success),
+                    Snackbar.LENGTH_SHORT
+                ).show()
+                true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
